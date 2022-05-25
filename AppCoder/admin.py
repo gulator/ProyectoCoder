1b54837fefa96383e362feb1e34cd45707fed303
Julian Dascanio <julian.dascanio@gmail.com>
2022-05-24 23:47:24 -0300

    sitio con admin y herencias html

--- AppCoder/admin.py
+++ AppCoder/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from AppCoder.views import profesor
+from .models import *
 
 # Register your models here.
+admin.site.register(Curso)
+admin.site.register(Estudiante)
+admin.site.register(Porfesor)
+admin.site.register(Entregable)
